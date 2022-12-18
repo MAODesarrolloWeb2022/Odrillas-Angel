9a28497c06cf5fdede2ed757950099004528ff6f
MAODesarrolloWeb2022 <115120792+MAODesarrolloWeb2022@users.noreply.github.com>
2022-12-17 21:48:06 -0400

    Add files via upload

---
+++ ejercicio en clase 4/fdatos.php
@@ -0,0 +1,9 @@
+<form action="menu.php" method="post">
+    <label for="a">a</label>
+    <input type="number">
+    <br>
+    <label for="b">b</label>
+    <input type="number">
+    <br>
+    <input type="submit" value="operar">
+</form>
